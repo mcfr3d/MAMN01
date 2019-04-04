7c2f0858b96c6396d701050c9302d707966d80c3
Fredrik <nyberg_11@hotmail.com>
2019-04-04 14:44:59 +0200

    Added accelerometer

--- app/src/main/java/com/example/myinteractiveapp/Accelerometer.java
+++ app/src/main/java/com/example/myinteractiveapp/Accelerometer.java
@@ -2,15 +2,72 @@ package com.example.myinteractiveapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Accelerometer extends AppCompatActivity {
+public class Accelerometer extends AppCompatActivity implements SensorEventListener {
+
+    private boolean mInitialized;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    /**
+     * Called when the activity is first created.
+     */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer);
-        Intent intent = getIntent();
+        mInitialized = false;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        TextView tvX = (TextView) findViewById(R.id.x_axis);
+        TextView tvY = (TextView) findViewById(R.id.y_axis);
+        TextView tvZ = (TextView) findViewById(R.id.z_axis);
+        ImageView iv = (ImageView) findViewById(R.id.image);
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        if (!mInitialized) {
+            tvX.setText("0.0");
+            tvY.setText("0.0");
+            tvZ.setText("0.0");
+            mInitialized = true;
+        } else {
+
+            tvX.setText(Float.toString(x));
+            tvY.setText(Float.toString(y));
+            tvZ.setText(Float.toString(z));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+// can be safely ignored for this demo
     }
 }
